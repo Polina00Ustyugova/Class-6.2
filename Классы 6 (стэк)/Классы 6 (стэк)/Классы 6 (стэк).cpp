9508b960492b694bf4f8ca6bed179a1d962210e8
Polya <poina.ustyugova.04@mail.ru>
2023-05-08 19:01:04 +0000

    Class 6.2

---
+++ Классы 6 (стэк)/Классы 6 (стэк)/Классы 6 (стэк).cpp
@@ -0,0 +1,46 @@
+﻿#include "Stack.h"
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() 
+{
+	setlocale(LC_ALL, "ru");
+	int n;
+	cout << "Введите размер стэка: ";
+	cin >> n;
+	Stack a(n);
+	int num;
+	cout << a << endl;
+	cout << "1. Удалить элемент из стэка\n";
+	cout << "2. Добавить элемент в стэка\n";
+	cout << "3. Вывести стэк на экран\n";
+	cout << "4. Выход\n";
+	int c = 0;
+	while (c != 4)
+	{
+		cout << "Выберите операцию: ";
+		cin >> c;
+		switch (c)
+		{
+		case 1:
+			a.pop();
+			break;
+		case 2:
+			cout << "Введите число, которое хотите добавить в стэк: ";
+			cin >> num;
+			a.push(num);
+			break;
+		case 3:
+			cout << a << endl;
+			break;
+		case 4:
+			return 0;
+			break;
+		default:
+			cout << "Введите номер операции корректно!";
+			break;
+		}
+	}
+	return 0;
+}
